fef10bb3a6480a0bb05488ffc2497eaab8a7c95a
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-09-17 14:21:44 +1000

    Added code to change class each time slot to past present or future based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@
 // in the html.
 
 var currentDay = dayjs();
-$('#currentDay').text(currentDay.format('dddd, MMMM Do'));
 
 
 
@@ -21,9 +20,30 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  var currentHour = currentDay.format('H');
+  // console.log(currentHour);
+  // console.log(typeof(currentHour));
+
+  for (var i = 9; i <= 17; i++){
+    if (i < currentHour){
+      // turn gray (past)
+      $('#hour-'+i).addClass('past')
+    } else if (i == currentHour){
+      // turn red (present)
+      $('#hour-'+i).addClass('present')
+    } else {
+      // turn green (future)
+      $('#hour-'+i).addClass('future')
+    }
+  }
+
+
+ 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  $('#currentDay').text(currentDay.format('dddd, MMMM Do'));
 });
